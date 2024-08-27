a6fa013543d6663ae0ed5a02c589e907a1c26447
Faina <Faika99@yandex.ru>
2024-08-27 19:27:19 +0300

    rename

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests1 {
+public class SearchTests {
     @Test
     void successfulSearchTest() {
         Configuration.holdBrowserOpen = true;
